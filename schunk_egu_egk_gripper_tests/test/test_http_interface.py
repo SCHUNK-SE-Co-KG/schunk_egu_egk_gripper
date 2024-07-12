405641d8faee779bf2c289bb1626048716ef5ad7
Stefan Scherzinger <stefan.scherzinger@de.schunk.com>
2024-07-12 16:07:01 +0200

    Make the dummy available for gripper tests
    - Move the launch fixture into `conftest.py`.
      This will probably be used by several test files.
    
    - Add a test to check whether the gripper's joint state topic gets
      published.

---
+++ schunk_egu_egk_gripper_tests/test/test_http_interface.py
@@ -0,0 +1,29 @@
+import pytest
+from test.conftest import launch_description
+import time
+from threading import Thread, Event
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import JointState
+from typing import Any
+
+
+class CheckTopic(Node):
+    def __init__(self, topic: str, type: Any):
+        super().__init__("check_topic")
+        self.event = Event()
+        self.sub = self.create_subscription(type, topic, self.msg_cb, 3)
+        self.thread = Thread(target=lambda node: rclpy.spin(node), args=(self,))
+        self.thread.start()
+
+    def msg_cb(self, data: Any) -> None:
+        self.event.set()
+
+
+@pytest.mark.launch(fixture=launch_description)
+def test_driver_connnects_to_gripper_dummy(launch_context, isolated, gripper_dummy):
+    until_dummy_ready = 3
+    timeout = 3
+
+    time.sleep(until_dummy_ready)
+    assert CheckTopic("/joint_states", JointState).event.wait(timeout)
--- schunk_egu_egk_gripper_tests/test/test_launch.py
+++ schunk_egu_egk_gripper_tests/test/test_launch.py
@@ -1,31 +1,12 @@
 #!/usr/bin/env python3
 import pytest
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.substitutions import PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
-import launch_pytest
-
 from rclpy.node import Node
 import time
-
-
-@launch_pytest.fixture
-def launch_description():
-    setup = IncludeLaunchDescription(
-        PathJoinSubstitution(
-            [
-                FindPackageShare("schunk_egu_egk_gripper_driver"),
-                "launch",
-                "schunk.launch.py",
-            ]
-        )
-    )
-    return LaunchDescription([setup, launch_pytest.actions.ReadyToTest()])
+from test.conftest import launch_description
 
 
 @pytest.mark.launch(fixture=launch_description)
-def test_normal_startup_works(launch_context, isolated):
+def test_normal_startup_works(launch_context, isolated, gripper_dummy):
     node = Node("test_startup")
     until_ready = 2.0  # sec
     time.sleep(until_ready)
