4443a327e3bb431de781f986f24f85d74bca9540
Stefan Scherzinger <stefan.scherzinger@de.schunk.com>
2025-02-22 10:52:35 +0100

    Make input output buffer access thread-safe
    It's really hard to come up with a failing test for this.
    Nevertheless, it shows that there are no deadlocks.

--- schunk_gripper_library/src/driver.py
+++ schunk_gripper_library/src/driver.py
@@ -1,4 +1,5 @@
 import struct
+from threading import Lock
 
 
 class Driver(object):
@@ -14,93 +15,121 @@ class Driver(object):
 
         self.plc_input_buffer = bytearray(bytes.fromhex("00" * 16))
         self.plc_output_buffer = bytearray(bytes.fromhex("00" * 16))
+        self.input_buffer_lock = Lock()
+        self.output_buffer_lock = Lock()
+
+    def set_plc_input(self, buffer: str) -> None:
+        with self.input_buffer_lock:
+            self.plc_input_buffer = bytearray(bytes.fromhex(buffer))
 
     def get_plc_input(self):
-        return self.plc_input_buffer.hex().upper()
+        with self.input_buffer_lock:
+            return self.plc_input_buffer.hex().upper()
+
+    def set_plc_output(self, buffer: str) -> None:
+        with self.output_buffer_lock:
+            self.plc_output_buffer = bytearray(bytes.fromhex(buffer))
 
     def get_plc_output(self):
-        return self.plc_output_buffer.hex().upper()
+        with self.output_buffer_lock:
+            return self.plc_output_buffer.hex().upper()
 
     def set_control_bit(self, bit: int, value: bool) -> bool:
-        if bit < 0 or bit > 31:
-            return False
-        if bit in self.reserved_control_bits:
-            return False
-        byte_index, bit_index = divmod(bit, 8)
-        if value:
-            self.plc_output_buffer[byte_index] |= 1 << bit_index
-        else:
-            self.plc_output_buffer[byte_index] &= ~(1 << bit_index)
-        return True
+        with self.output_buffer_lock:
+            if bit < 0 or bit > 31:
+                return False
+            if bit in self.reserved_control_bits:
+                return False
+            byte_index, bit_index = divmod(bit, 8)
+            if value:
+                self.plc_output_buffer[byte_index] |= 1 << bit_index
+            else:
+                self.plc_output_buffer[byte_index] &= ~(1 << bit_index)
+            return True
 
     def get_control_bit(self, bit: int) -> int | bool:
-        if bit < 0 or bit > 31:
-            return False
-        if bit in self.reserved_control_bits:
-            return False
-        byte_index, bit_index = divmod(bit, 8)
-        return 1 if self.plc_output_buffer[byte_index] & (1 << bit_index) != 0 else 0
+        with self.output_buffer_lock:
+            if bit < 0 or bit > 31:
+                return False
+            if bit in self.reserved_control_bits:
+                return False
+            byte_index, bit_index = divmod(bit, 8)
+            return (
+                1 if self.plc_output_buffer[byte_index] & (1 << bit_index) != 0 else 0
+            )
 
     def toggle_control_bit(self, bit: int) -> bool:
-        if bit < 0 or bit > 31:
-            return False
-        if bit in self.reserved_control_bits:
-            return False
-        byte_index, bit_index = divmod(bit, 8)
-        self.plc_output_buffer[byte_index] ^= 1 << bit_index
-        return True
+        with self.output_buffer_lock:
+            if bit < 0 or bit > 31:
+                return False
+            if bit in self.reserved_control_bits:
+                return False
+            byte_index, bit_index = divmod(bit, 8)
+            self.plc_output_buffer[byte_index] ^= 1 << bit_index
+            return True
 
     def get_status_bit(self, bit: int) -> int | bool:
-        if bit < 0 or bit > 31:
-            return False
-        if bit in self.reserved_status_bits:
-            return False
-        byte_index, bit_index = divmod(bit, 8)
-        return 1 if self.plc_input_buffer[byte_index] & (1 << bit_index) != 0 else 0
+        with self.input_buffer_lock:
+            if bit < 0 or bit > 31:
+                return False
+            if bit in self.reserved_status_bits:
+                return False
+            byte_index, bit_index = divmod(bit, 8)
+            return 1 if self.plc_input_buffer[byte_index] & (1 << bit_index) != 0 else 0
 
     def get_status_error(self) -> str:
-        return hex(self.plc_input_buffer[self.error_byte]).replace("0x", "").upper()
+        with self.input_buffer_lock:
+            return hex(self.plc_input_buffer[self.error_byte]).replace("0x", "").upper()
 
     def get_status_diagnostics(self) -> str:
-        return (
-            hex(self.plc_input_buffer[self.diagnostics_byte]).replace("0x", "").upper()
-        )
+        with self.input_buffer_lock:
+            return (
+                hex(self.plc_input_buffer[self.diagnostics_byte])
+                .replace("0x", "")
+                .upper()
+            )
 
     def set_target_position(self, target_pos: int) -> bool:
-        if not isinstance(target_pos, int):
-            return False
-        if target_pos < 0:
-            return False
-        self.plc_output_buffer[4:8] = bytes(struct.pack("i", target_pos))
-        return True
+        with self.output_buffer_lock:
+            if not isinstance(target_pos, int):
+                return False
+            if target_pos < 0:
+                return False
+            self.plc_output_buffer[4:8] = bytes(struct.pack("i", target_pos))
+            return True
 
     def get_target_position(self) -> int:  # um
-        return struct.unpack("i", self.plc_output_buffer[4:8])[0]
+        with self.output_buffer_lock:
+            return struct.unpack("i", self.plc_output_buffer[4:8])[0]
 
     def set_target_speed(self, target_speed: int) -> bool:
-        if not isinstance(target_speed, int):
-            return False
-        if target_speed < 0:
-            return False
-        self.plc_output_buffer[8:12] = bytes(struct.pack("i", target_speed))
-        return True
+        with self.output_buffer_lock:
+            if not isinstance(target_speed, int):
+                return False
+            if target_speed < 0:
+                return False
+            self.plc_output_buffer[8:12] = bytes(struct.pack("i", target_speed))
+            return True
 
     def get_target_speed(self) -> float:
-        return (
-            struct.unpack("i", self.plc_output_buffer[8:12])[0] / 1000.0
-        )  # um/s to mm/s
+        with self.output_buffer_lock:
+            return (
+                struct.unpack("i", self.plc_output_buffer[8:12])[0] / 1000.0
+            )  # um/s to mm/s
 
     def get_actual_position(self) -> int:  # um
-        return struct.unpack("i", self.plc_input_buffer[4:8])[0]
+        with self.input_buffer_lock:
+            return struct.unpack("i", self.plc_input_buffer[4:8])[0]
 
     def _set_status_bit(self, bit: int, value: bool) -> bool:
-        if bit < 0 or bit > 31:
-            return False
-        if bit in self.reserved_status_bits:
-            return False
-        byte_index, bit_index = divmod(bit, 8)
-        if value:
-            self.plc_input_buffer[byte_index] |= 1 << bit_index
-        else:
-            self.plc_input_buffer[byte_index] &= ~(1 << bit_index)
-        return True
+        with self.input_buffer_lock:
+            if bit < 0 or bit > 31:
+                return False
+            if bit in self.reserved_status_bits:
+                return False
+            byte_index, bit_index = divmod(bit, 8)
+            if value:
+                self.plc_input_buffer[byte_index] |= 1 << bit_index
+            else:
+                self.plc_input_buffer[byte_index] &= ~(1 << bit_index)
+            return True
--- schunk_gripper_library/tests/test_data_buffers.py
+++ schunk_gripper_library/tests/test_data_buffers.py
@@ -1,4 +1,5 @@
 from schunk_gripper_library.src.driver import Driver
+from threading import Thread
 
 
 def test_driver_initializes_plc_data_buffers():
@@ -15,3 +16,52 @@ def test_driver_supports_reading_plc_data_buffers():
     driver.plc_output_buffer = bytearray(bytes.fromhex(output_buffer))
     assert driver.get_plc_input() == input_buffer
     assert driver.get_plc_output() == output_buffer
+
+
+def test_driver_supports_writing_plc_data_buffers():
+    driver = Driver()
+    input_buffer = "0011223344556677".upper() * 2
+    output_buffer = "8899aabbccddeeff".upper() * 2
+    driver.set_plc_input(input_buffer)
+    driver.set_plc_output(output_buffer)
+    assert driver.plc_input_buffer.hex().upper() == input_buffer
+    assert driver.plc_output_buffer.hex().upper() == output_buffer
+
+
+def test_accessing_input_output_buffers_is_threadsafe():
+    # Write concurrently into the data buffers and check whether
+    # their content is consistent
+
+    driver = Driver()
+    nr_accesses = 10000
+    all_zeros = "00" * 16
+    all_ones = "FF" * 16
+
+    def write(buffer: str):
+        for _ in range(nr_accesses):
+            driver.set_plc_input(buffer)
+            driver.set_plc_output(buffer)
+
+    def check_data_integrity():
+        for _ in range(nr_accesses):
+            input = driver.get_plc_input()
+            output = driver.get_plc_output()
+            assert input == all_zeros or input == all_ones
+            assert output == all_zeros or output == all_ones
+
+    updating_threads = []
+    for i in range(10):
+        if i % 2 == 0:
+            buffer = all_zeros
+        else:
+            buffer = all_ones
+        thread = Thread(target=write, args=(buffer,))
+        thread.start()
+        updating_threads.append(thread)
+
+    client_thread = Thread(target=check_data_integrity)
+    client_thread.start()
+
+    client_thread.join()
+    for thread in updating_threads:
+        thread.join()
