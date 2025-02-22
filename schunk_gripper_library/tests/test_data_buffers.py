3556d7715fd1beaf55b107e4d9e15604ffdbe579
Stefan Scherzinger <stefan.scherzinger@de.schunk.com>
2025-02-22 14:19:19 +0100

    Move threading-related tests into a separate module

--- schunk_gripper_library/tests/test_data_buffers.py
+++ schunk_gripper_library/tests/test_data_buffers.py
@@ -1,5 +1,4 @@
 from schunk_gripper_library.src.driver import Driver
-from threading import Thread
 
 
 def test_driver_initializes_plc_data_buffers():
@@ -26,42 +25,3 @@ def test_driver_supports_writing_plc_data_buffers():
     driver.set_plc_output(output_buffer)
     assert driver.plc_input_buffer.hex().upper() == input_buffer
     assert driver.plc_output_buffer.hex().upper() == output_buffer
-
-
-def test_accessing_input_output_buffers_is_threadsafe():
-    # Write concurrently into the data buffers and check whether
-    # their content is consistent
-
-    driver = Driver()
-    nr_accesses = 10000
-    all_zeros = "00" * 16
-    all_ones = "FF" * 16
-
-    def write(buffer: str):
-        for _ in range(nr_accesses):
-            driver.set_plc_input(buffer)
-            driver.set_plc_output(buffer)
-
-    def check_data_integrity():
-        for _ in range(nr_accesses):
-            input = driver.get_plc_input()
-            output = driver.get_plc_output()
-            assert input == all_zeros or input == all_ones
-            assert output == all_zeros or output == all_ones
-
-    updating_threads = []
-    for i in range(10):
-        if i % 2 == 0:
-            buffer = all_zeros
-        else:
-            buffer = all_ones
-        thread = Thread(target=write, args=(buffer,))
-        thread.start()
-        updating_threads.append(thread)
-
-    client_thread = Thread(target=check_data_integrity)
-    client_thread.start()
-
-    client_thread.join()
-    for thread in updating_threads:
-        thread.join()
