66dddb0e2f687834c428825c7a3b72e2533c9c51
Stefan Scherzinger <stefan.scherzinger@de.schunk.com>
2025-03-15 16:54:56 +0100

    Make sure *.json config files are installed
    Also add the `httpx` dependency.

--- schunk_gripper_library/setup.py
+++ schunk_gripper_library/setup.py
@@ -8,6 +8,7 @@ setup(
     name=package_name,
     version="0.0.1",
     packages=find_packages(exclude=["tests"]),
+    include_package_data=True,
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
@@ -16,7 +17,7 @@ setup(
             glob(package_name + "/config/*.json"),
         ),
     ],
-    install_requires=["setuptools", "pymodbus", "pyserial"],
+    install_requires=["setuptools", "pymodbus", "pyserial", "httpx"],
     zip_safe=True,
     author="Stefan Scherzinger",
     author_email="stefan.scherzinger@de.schunk.com",
