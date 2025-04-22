07f88ca55e932c00635e1a6d70c8283bff134726
Jan Mack <jan.mack@de.schunk.com>
2025-04-22 09:49:02 +0200

    Clean up grip workpiece test

--- schunk_gripper_library/tests/test_behavior.py
+++ schunk_gripper_library/tests/test_behavior.py
@@ -132,52 +132,18 @@ def test_grip_workpiece():
     test_gpe = True
     test_vel = 0
     driver = Driver()
-
-    # Not connected
-    assert not asyncio.run(
-        driver.grip_workpiece(
-            gripping_force=test_force, use_gpe=test_gpe, grip_inside=True
+    for host, port, serial_port in zip(
+        ["0.0.0.0", None], [8000, None], [None, "/dev/ttyUSB0"]
+    ):
+        # Not connected
+        assert not asyncio.run(
+            driver.grip_workpiece(
+                gripping_force=test_force, use_gpe=test_gpe, grip_inside=True
+            )
         )
-    )
 
-    # Connection to the Webserver
-    assert driver.connect(host="0.0.0.0", port=8000, serial_port=None)
-
-    assert asyncio.run(driver.acknowledge())
-
-    assert not asyncio.run(
-        driver.grip_workpiece(
-            gripping_force=test_force,
-            use_gpe=test_gpe,
-            grip_inside=False,
-            grip_outside=False,
-        )
-    )
-    assert not asyncio.run(
-        driver.grip_workpiece(
-            gripping_force=test_force,
-            use_gpe=test_gpe,
-            grip_inside=True,
-            grip_outside=True,
-        )
-    )
-
-    assert asyncio.run(
-        driver.grip_workpiece(
-            gripping_force=test_force,
-            use_gpe=test_gpe,
-            grip_inside=True,
-            gripping_velocity=test_vel,
-        )
-    )
-
-    assert driver.disconnect()
-
-    """
-    try:
-        # Connection to the Modbus
         assert driver.connect(
-            host=None, port=None, serial_port="/dev/ttyUSB0", device_id=12
+            host=host, port=port, serial_port=serial_port, device_id=12
         )
 
         assert asyncio.run(driver.acknowledge())
@@ -190,17 +156,13 @@ def test_grip_workpiece():
                 grip_outside=False,
             )
         )
-        assert asyncio.run(driver.acknowledge())
-        assert not asyncio.run(
-            driver.grip_workpiece(
-                gripping_force=test_force,
-                use_gpe=test_gpe,
-                grip_inside=True,
-                grip_outside=True,
-            )
-        )
-        assert asyncio.run(driver.acknowledge())
-        asyncio.run(
+
+        # Modbus testing not possible, hence this skip
+        if serial_port:
+            assert driver.disconnect()
+            return
+
+        assert asyncio.run(
             driver.grip_workpiece(
                 gripping_force=test_force,
                 use_gpe=test_gpe,
@@ -209,17 +171,7 @@ def test_grip_workpiece():
             )
         )
 
-        driver.print_bits([3,6,11,12,4,16])
-        desired_bits = {"11":1}
-        time.sleep(4)
-        assert asyncio.run(driver.wait_for_status(bits=desired_bits))
-
         assert driver.disconnect()
-    except Exception as e:
-        raise e
-    finally:
-        driver.disconnect()
-    """
 
 
 @skip_without_gripper
