9359d0e3cfdd5c6261b4f7a19b033f692b1fc9a4
Jan Mack <jan.mack@de.schunk.com>
2025-05-19 10:47:39 +0200

    Add log_level to parameter tests and create tests to change the log_level parameter

--- schunk_gripper_driver/schunk_gripper_driver/tests/test_logging.py
+++ schunk_gripper_driver/schunk_gripper_driver/tests/test_logging.py
@@ -17,6 +17,9 @@ from schunk_gripper_library.utility import skip_without_gripper
 from lifecycle_msgs.msg import Transition
 import time
 import pytest
+from rcl_interfaces.srv import GetParameters, SetParameters
+from rcl_interfaces.msg import Parameter, ParameterValue, ParameterType
+import rclpy
 
 LOG_SIZE_BYTES = 500  # Minimal output for driver startup and shutdown
 
@@ -39,3 +42,83 @@ def test_driver_doesnt_fill_disk_space_by_default(log_monitor, lifecycle_interfa
         driver.change_state(Transition.TRANSITION_CLEANUP)
 
     # log_monitor does the final asserting
+
+
+@skip_without_gripper
+def test_driver_supports_log_level_changes_via_parameter_call(driver):
+    client = rclpy.create_node("test_log_level")
+    set_params_client = client.create_client(
+        SetParameters, "/schunk/driver/set_parameters"
+    )
+    assert set_params_client.wait_for_service(timeout_sec=2)
+    get_params_client = client.create_client(
+        GetParameters, "/schunk/driver/get_parameters"
+    )
+    assert get_params_client.wait_for_service(timeout_sec=2)
+    future = set_params_client.call_async(
+        SetParameters.Request(
+            parameters=[
+                Parameter(
+                    name="log_level",
+                    value=ParameterValue(
+                        type=ParameterType.PARAMETER_STRING, string_value="DEBUG"
+                    ),
+                )
+            ]
+        )
+    )
+    rclpy.spin_until_future_complete(client, future)
+    future = get_params_client.call_async(GetParameters.Request(names=["log_level"]))
+    rclpy.spin_until_future_complete(client, future)
+    assert future.result().values[0].string_value == "DEBUG"
+    future = set_params_client.call_async(
+        SetParameters.Request(
+            parameters=[
+                Parameter(
+                    name="log_level",
+                    value=ParameterValue(
+                        type=ParameterType.PARAMETER_STRING, string_value="INFO"
+                    ),
+                )
+            ]
+        )
+    )
+    rclpy.spin_until_future_complete(client, future)
+    future = get_params_client.call_async(GetParameters.Request(names=["log_level"]))
+    rclpy.spin_until_future_complete(client, future)
+    assert future.result().values[0].string_value == "INFO"
+
+
+@skip_without_gripper
+def test_driver_doesnt_set_invalid_log_level(driver):
+    client = rclpy.create_node("test_bad_log_level")
+    set_params_client = client.create_client(
+        SetParameters, "/schunk/driver/set_parameters"
+    )
+    assert set_params_client.wait_for_service(timeout_sec=2)
+    get_params_client = client.create_client(
+        GetParameters, "/schunk/driver/get_parameters"
+    )
+    assert get_params_client.wait_for_service(timeout_sec=2)
+
+    # Attempt to set an invalid log level
+    future = set_params_client.call_async(
+        SetParameters.Request(
+            parameters=[
+                Parameter(
+                    name="log_level",
+                    value=ParameterValue(
+                        type=ParameterType.PARAMETER_STRING,
+                        string_value="BAD_LOG_LEVEL",
+                    ),
+                )
+            ]
+        )
+    )
+    rclpy.spin_until_future_complete(client, future, timeout_sec=2)
+    print(future.result())
+    assert future.result().results[0].successful is False
+    # Verify the log level was not set to the invalid value
+    future = get_params_client.call_async(GetParameters.Request(names=["log_level"]))
+    rclpy.spin_until_future_complete(client, future)
+    assert future.result().values[0].string_value != "BAD_LOG_LEVEL"
