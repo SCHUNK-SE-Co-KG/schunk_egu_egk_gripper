7e271d76fe5131ae21de11cdb3b59d30a9d853ce
Jan Mack <jan.mack@de.schunk.com>
2025-05-26 16:35:31 +0200

    Add functions to scan for devices in Modbus network

--- schunk_gripper_library/schunk_gripper_library/tests/test_scanner.py
+++ schunk_gripper_library/schunk_gripper_library/tests/test_scanner.py
@@ -1,28 +1,58 @@
 from schunk_gripper_library.driver import Driver
 from schunk_gripper_library.utility import skip_without_gripper, Scanner
+import asyncio
 
 
 @skip_without_gripper
-def test_scanner_returns_list_of_IDs():
+def test_scanner_returns_list_of_IDs(time_execution):
+    start_address = 1
+    end_address = 20
     driver = Driver()
 
     driver.connect(serial_port="/dev/ttyUSB0", device_id=12)
-    scanner = Scanner()
+    scanner = Scanner(driver)
 
-    grippers = scanner.scan(start_address=1, end_address=50)
+    result = asyncio.run(
+        scanner.scan(start_address=start_address, end_address=end_address)
+    )
 
-    assert isinstance(grippers, list)
-    assert len(grippers) != 0
-    for item in grippers:
+    assert isinstance(result, list)
+    assert len(result) != 0
+    for item in result:
         assert isinstance(item, int)
 
 
 @skip_without_gripper
 def test_scanner_with_specific_device_id():
-    scanner = Scanner()
+    import time
+
+    time.sleep(5)
+    start_address = 1
+    end_address = 20
+
+    driver = Driver()
+
+    driver.connect(serial_port="/dev/ttyUSB0", device_id=12)
+    scanner = Scanner(driver)
 
     device_id = 12
 
-    grippers = scanner.scan(start_address=device_id, end_address=device_id)
+    grippers = asyncio.run(
+        scanner.scan(start_address=start_address, end_address=end_address)
+    )
 
     assert device_id in grippers
+
+
+@skip_without_gripper
+def test_scanner_scans_specified_number_of_grippers():
+    driver = Driver()
+
+    driver.connect(serial_port="/dev/ttyUSB0", device_id=12)
+    scanner = Scanner(driver)
+
+    num_grippers = 1
+
+    result = asyncio.run(scanner.scan(gripper_num=num_grippers))
+
+    assert len(result) == num_grippers
