fa1d4b31d5e93a0b2352283b9dcb050b97392780
PrajwalMangaluru <prajwal.mangaluru@de.schunk.com>
2025-05-30 16:27:20 +0200

    Make code compliant with pre-commit hooks

--- schunk_gripper_driver/schunk_gripper_driver/driver.py
+++ schunk_gripper_driver/schunk_gripper_driver/driver.py
@@ -27,12 +27,11 @@ from schunk_gripper_interfaces.srv import (  # type: ignore [attr-defined]
     Grip,
     Release,
     ShowConfiguration,
-    ShowGripperSpecification
+    ShowGripperSpecification,
 )
 from schunk_gripper_interfaces.msg import (  # type: ignore [attr-defined]
     Gripper as GripperConfig,
     GripperState,
-    GripperSpec,
 )
 from sensor_msgs.msg import JointState
 from std_srvs.srv import Trigger
@@ -575,32 +574,33 @@ class Driver(Node):
         return response
 
     def _show_gripper_specification_cb(
-        self, 
-        request: ShowGripperSpecification.Request, 
+        self,
+        request: ShowGripperSpecification.Request,
         response: ShowGripperSpecification.Response,
         gripper: Gripper,
     ):
-        self.get_logger().info("---> Show Specification callback")
+        self.get_logger().info("---> Show Specification")
         if self.needs_synchronize(gripper):
             spec = asyncio.run(
                 gripper["driver"].show_gripper_specification(
-                scheduler = self.scheduler,
+                    scheduler=self.scheduler,
                 )
             )
         else:
-            spec = asyncio.run(
-                gripper["driver"].show_gripper_specification(
-                )
-            )
-        response.success = spec is not False
+            spec = asyncio.run(gripper["driver"].show_gripper_specification())
+        response.success = spec is not None
         response.message = gripper["driver"].get_status_diagnostics()
-        if spec:
-            response.specification.max_stroke = spec["max_stroke"]
-            response.specification.max_speed = spec["max_speed"]
-            response.specification.max_force = spec["max_force"]
-            response.specification.serial_number = spec["serial_number"]
-            response.specification.firmware_version = spec["firmware_version"]
+        if isinstance(spec, dict):
+            response.specification.max_stroke = spec.get("max_stroke", 0.0)
+            response.specification.max_speed = spec.get("max_speed", 0.0)
+            response.specification.max_force = spec.get("max_force", 0.0)
+            response.specification.serial_number = spec.get("serial_number", "")
+            response.specification.firmware_version = spec.get("firmware_version", "")
+            response.success = True
+        else:
+            response.success = False
         return response
+
 
 def main():
     rclpy.init()
