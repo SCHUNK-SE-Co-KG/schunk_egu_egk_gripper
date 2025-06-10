9ac893af2270fee42438fdc6e51b6064586d0ae9
PrajwalMangaluru <prajwal.mangaluru@de.schunk.com>
2025-06-10 17:50:47 +0200

    Always return a dict in show_gripper_specification()
    Return a dict on function call, empty dict if function call fails

--- schunk_gripper_driver/schunk_gripper_driver/driver.py
+++ schunk_gripper_driver/schunk_gripper_driver/driver.py
@@ -582,16 +582,16 @@ class Driver(Node):
         self.get_logger().info("---> Show Specification")
 
         spec = gripper["driver"].show_gripper_specification()
-        if not isinstance(spec, dict):
+        if not spec:
             response.success = False
             response.message = gripper["driver"].get_status_diagnostics()
             return response
 
-        response.specification.max_stroke = spec.get("max_stroke", 0.0)
-        response.specification.max_speed = spec.get("max_speed", 0.0)
-        response.specification.max_force = spec.get("max_force", 0.0)
-        response.specification.serial_number = spec.get("serial_number", "")
-        response.specification.firmware_version = spec.get("firmware_version", "")
+        response.specification.max_stroke = spec["max_stroke"]
+        response.specification.max_speed = spec["max_speed"]
+        response.specification.max_force = spec["max_force"]
+        response.specification.serial_number = spec["serial_number"]
+        response.specification.firmware_version = spec["firmware_version"]
         response.success = True
         response.message = gripper["driver"].get_status_diagnostics()
         return response
