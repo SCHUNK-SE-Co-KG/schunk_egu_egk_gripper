6eee2b7ba8375e5f0ee89fdaa85daa0595b3890c
Stefan Scherzinger <stefan.scherzinger@de.schunk.com>
2025-07-08 10:14:50 +0200

    Remove the Modbus dummy
    We are no longer using this.

--- schunk_gripper_library/schunk_gripper_library/tests/conftest.py
+++ schunk_gripper_library/schunk_gripper_library/tests/conftest.py
@@ -1,8 +1,5 @@
 import pytest
 from .etc.pseudo_terminals import Connection
-from .etc.modbus_server import ModbusServer
-import asyncio
-import threading
 
 
 @pytest.fixture(scope="module")
@@ -15,34 +12,3 @@ def pseudo_terminals():
 
     print("Closing both pseudo terminals")
     connection.close()
-
-
-@pytest.fixture(scope="module")
-def modbus_server(pseudo_terminals):
-    pt1, pt2 = pseudo_terminals
-
-    async def periodic_modbus_task(pt1, stop_event):
-        server = ModbusServer()
-        await server.setup(port=pt1)
-        await server.start()
-
-        try:
-            while not stop_event.is_set():
-                await asyncio.sleep(1)
-        finally:
-            server.stop()
-
-    loop = asyncio.new_event_loop()
-    thread = threading.Thread(target=lambda: loop.run_forever())
-    thread.start()
-
-    stop_event = asyncio.Event()
-    task = asyncio.run_coroutine_threadsafe(periodic_modbus_task(pt1, stop_event), loop)
-    yield pt2
-
-    stop_event.set()
-    task.result()
-
-    loop.call_soon_threadsafe(loop.stop)
-    thread.join()
-    loop.close()
